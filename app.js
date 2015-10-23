0cee2e68c46bee944f83ad14ce17d78b44f1680e
Syamil <syamil8@gmail.com>
2015-10-23 09:10:30 +0800

    added meow, removed everything else

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var jf = require('jsonfile');
 var TelegramBot = require('node-telegram-bot-api');
 var chalk = require('chalk');
-var readline = require('readline');
+// var readline = require('readline');
 var credentialsFilePath = './private/telegram_credentials.json';
-var logger = require('./logger');
-var weather = require('./weather');
-var traffic = require('./traffic');
-var feedback = require('./feedback');
-var do_not_open = require('./do_not_open');
-var broadcast = require('./broadcast');
+// var logger = require('./logger');
+// var weather = require('./weather');
+// var traffic = require('./traffic');
+// var feedback = require('./feedback');
+// var do_not_open = require('./do_not_open');
+// var broadcast = require('./broadcast');
 
 var CREDENTIALS = jf.readFileSync(credentialsFilePath);
 var bot = new TelegramBot(CREDENTIALS.token, {
@@ -17,7 +17,7 @@ var bot = new TelegramBot(CREDENTIALS.token, {
 
 console.log(chalk.blue("============================"));
 console.log(chalk.blue("                            "));
-console.log(chalk.blue("      CinnaBot Started      "));
+console.log(chalk.blue("      CinnaCat Started      "));
 console.log(chalk.blue("                            "));
 console.log(chalk.blue("============================"));
 console.log(chalk.blue("                            "));
@@ -26,25 +26,25 @@ var sessions = {};
 var session;
 
 // start CLI app
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-});
+// var rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//     terminal: false
+// });
 
-rl.on('line', function(line) {
-    parseCommand(line);
-});
+// rl.on('line', function(line) {
+//     parseCommand(line);
+// });
 
-function parseCommand(command) {
-    chatIds = [49892469, 102675141];
-    switch (command) {
-        case 'bcast':
-            return broadcast.broadcast_input(chatIds, bot);
-        case 'exit':
-            return process.exit();
-    }
-}
+// function parseCommand(command) {
+//     chatIds = [49892469, 102675141];
+//     switch (command) {
+//         case 'bcast':
+//             return broadcast.broadcast_input(chatIds, bot);
+//         case 'exit':
+//             return process.exit();
+//     }
+// }
 
 // Any kind of message
 bot.on('message', function(msg) {
@@ -60,130 +60,130 @@ bot.on('message', function(msg) {
     }
     // manage commands
     switch (command) {
-        case "psi":
-            return psi(chatId);
-        case "bus":
+        case "meow":
+            return meow(chatId);
+        /*case "bus":
             var busstop = args;
             return bus(msgId, chatId, busstop);
         case "feedback":
             session = sessions[chatId] || new Session(chatId);
             return ask_dining_feedback(chatId);
         case "cat":
-            return catfact(chatId);
+            return catfact(chatId);*/
         case "cancel":
             return cancel(chatId);
     }
 
     // manage markups
-    switch (body) {
-        case 'I wanna go Buona Vista':
-            return bus(msgId, chatId, 19051);
-        case 'I wanna go Clementi':
-            return bus(msgId, chatId, 19059);
-        default:
-            session = sessions[chatId] || new Session(chatId);
-            if (session.inThread.status) {
-                return session.inThread.next(body, chatId);
-            }
-            return default_msg(chatId);
-    }
+    // switch (body) {
+    //     case 'I wanna go Buona Vista':
+    //         return bus(msgId, chatId, 19051);
+    //     case 'I wanna go Clementi':
+    //         return bus(msgId, chatId, 19059);
+    //     default:
+    //         session = sessions[chatId] || new Session(chatId);
+    //         if (session.inThread.status) {
+    //             return session.inThread.next(body, chatId);
+    //         }
+    //         return default_msg(chatId);
+    // }
 });
 
-function cancel(chatId) {
-    session = new Session(chatId);
-    bot.sendMessage(chatId, "Canceled.");
+function meow(chatId) {
+    //session = new Session(chatId);
+    bot.sendMessage(chatId, "Meow");
 }
 
-function catfact(chatId) {
-    return do_not_open.catfact(chatId, bot);
-}
+// function catfact(chatId) {
+//     return do_not_open.catfact(chatId, bot);
+// }
 
-function ask_dining_feedback(chatId) {
-    session.inThread.status = true;
-    feedback.ask_when_dining_feedback(chatId, bot);
-    session.inThread.next = ask_where_dining_feedback;
-}
+// function ask_dining_feedback(chatId) {
+//     session.inThread.status = true;
+//     feedback.ask_when_dining_feedback(chatId, bot);
+//     session.inThread.next = ask_where_dining_feedback;
+// }
 
-function ask_where_dining_feedback(when, chatId) {
-    var validOptions = ["Breakfast", "Dinner"];
-    if (validOptions.indexOf(when) < 0) {
-        return ask_dining_feedback(chatId);
-    }
-    session.diningFeedback.when = when;
-    feedback.ask_where_dining_feedback(chatId, bot, when);
-    session.inThread.next = ask_how_dining_feedback;
-}
+// function ask_where_dining_feedback(when, chatId) {
+//     var validOptions = ["Breakfast", "Dinner"];
+//     if (validOptions.indexOf(when) < 0) {
+//         return ask_dining_feedback(chatId);
+//     }
+//     session.diningFeedback.when = when;
+//     feedback.ask_where_dining_feedback(chatId, bot, when);
+//     session.inThread.next = ask_how_dining_feedback;
+// }
 
-function ask_how_dining_feedback(where, chatId) {
-    var df = session.diningFeedback;
-    var validOptions = [];
-    if (df.when === "Breakfast") {
-        validOptions = ['Asian', 'Western', 'Muslim', 'Toast', 'Other'];
-    } else if (df.when === "Dinner") {
-        validOptions = ['Noodle', 'Asian', 'Western - Main Course', 'Western - Panini', 'Indian', 'Malay', 'Late Plate'];
-    }
-    if (validOptions.indexOf(where) < 0) {
-        return ask_where_dining_feedback(df.when, chatId);
-    }
-    df.where = where;
-    feedback.ask_how_dining_feedback(chatId, bot);
-    session.inThread.next = done_dining_feedback;
-}
+// function ask_how_dining_feedback(where, chatId) {
+//     var df = session.diningFeedback;
+//     var validOptions = [];
+//     if (df.when === "Breakfast") {
+//         validOptions = ['Asian', 'Western', 'Muslim', 'Toast', 'Other'];
+//     } else if (df.when === "Dinner") {
+//         validOptions = ['Noodle', 'Asian', 'Western - Main Course', 'Western - Panini', 'Indian', 'Malay', 'Late Plate'];
+//     }
+//     if (validOptions.indexOf(where) < 0) {
+//         return ask_where_dining_feedback(df.when, chatId);
+//     }
+//     df.where = where;
+//     feedback.ask_how_dining_feedback(chatId, bot);
+//     session.inThread.next = done_dining_feedback;
+// }
 
-function done_dining_feedback(how, chatId) {
-    var df = session.diningFeedback;
-    var validOptions = ['👍', '👍👍', '👍👍👍', '👍👍👍👍', '👍👍👍👍👍'];
-    if (validOptions.indexOf(how) < 0) {
-        return ask_how_dining_feedback(df.where, chatId);
-    }
-    df.how = how.length / 2;
-    feedback.dining_feedback(chatId, bot, df.when, df.where, df.how);
-    session = new Session(chatId);
-}
+// function done_dining_feedback(how, chatId) {
+//     var df = session.diningFeedback;
+//     var validOptions = ['👍', '👍👍', '👍👍👍', '👍👍👍👍', '👍👍👍👍👍'];
+//     if (validOptions.indexOf(how) < 0) {
+//         return ask_how_dining_feedback(df.where, chatId);
+//     }
+//     df.how = how.length / 2;
+//     feedback.dining_feedback(chatId, bot, df.when, df.where, df.how);
+//     session = new Session(chatId);
+// }
 
-function Session(chatId) {
-    this.chatId = chatId;
-    this.inThread = {
-        "status": false,
-        "next": ask_where_dining_feedback
-    };
-    this.diningFeedback = new DiningFeedback();
-    sessions[chatId] = this;
-}
+// function Session(chatId) {
+//     this.chatId = chatId;
+//     this.inThread = {
+//         "status": false,
+//         "next": ask_where_dining_feedback
+//     };
+//     this.diningFeedback = new DiningFeedback();
+//     sessions[chatId] = this;
+// }
 
-function DiningFeedback() {
-    this.when = "";
-    this.where = "";
-    this.how = -1;
-}
+// function DiningFeedback() {
+//     this.when = "";
+//     this.where = "";
+//     this.how = -1;
+// }
 
-function psi(chatId) {
-    bot.sendMessage(chatId, weather.getWeather());
-}
+// function psi(chatId) {
+//     bot.sendMessage(chatId, weather.getWeather());
+// }
 
-function bus(msgId, chatId, busstop) {
-    function basicCallback(data) {
-        bot.sendMessage(chatId, data);
-    }
+// function bus(msgId, chatId, busstop) {
+//     function basicCallback(data) {
+//         bot.sendMessage(chatId, data);
+//     }
 
-    function callback(data) {
-        var opts = {
-            reply_markup: JSON.stringify({
-                keyboard: [
-                    ['Towards Buona Vista'],
-                    ['Towards Clementi'],
-                ],
-                one_time_keyboard: true
-            })
-        };
-        bot.sendMessage(chatId, data, opts);
-    }
+//     function callback(data) {
+//         var opts = {
+//             reply_markup: JSON.stringify({
+//                 keyboard: [
+//                     ['Towards Buona Vista'],
+//                     ['Towards Clementi'],
+//                 ],
+//                 one_time_keyboard: true
+//             })
+//         };
+//         bot.sendMessage(chatId, data, opts);
+//     }
 
-    if (busstop) {
-        return traffic.busStopQuery(busstop, basicCallback);
-    }
-    callback("Choose Direction:");
-}
+//     if (busstop) {
+//         return traffic.busStopQuery(busstop, basicCallback);
+//     }
+//     callback("Choose Direction:");
+// }
 
 function default_msg(chatId) {
     bot.sendMessage(chatId, "Unknown Command.");
